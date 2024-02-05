99c6b66ba63c90eb54cfcf7c3a825222d897518e
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-02-05 09:48:47 +0100

    UsersController DynamicParameters

--- T101_ConsolidatedEndpoints/Controllers/UsersController.cs
+++ T101_ConsolidatedEndpoints/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
 using System.Globalization;
 using T101_ConsolidatedEndpoints.Data;
 using T101_ConsolidatedEndpoints.Models;
@@ -20,21 +22,24 @@ namespace T101_ConsolidatedEndpoints.Controllers
 		public IEnumerable<UserModel> GetUser(int userId, bool isActive)
 		{
 			string sql = "EXEC TutorialAppSchema.spUsers_Get";
-			string parameters = "";
+			string stringParameters = "";
+			DynamicParameters sqlParameters = new();
 
 			if (userId != 0)
 			{
-				parameters += $", @UserId={userId}";
+				stringParameters += ", @UserId=@UserIdParam";
+				sqlParameters.Add("@UserIdParam", userId, DbType.Int32);
 			}
 
 			if (isActive)
 			{
-				parameters += $", @Active={isActive}";
+				stringParameters += ", @Active=@ActiveParam";
+				sqlParameters.Add("@ActiveParam", isActive, DbType.Boolean);
 			}
 
-			sql += parameters[1..];
+			sql += stringParameters[1..];
 
-			IEnumerable<UserModel> users = _dapper.LoadData<UserModel>(sql);
+			IEnumerable<UserModel> users = _dapper.LoadDataWithParameters<UserModel>(sql, sqlParameters);
 
 			return users;
 		}
@@ -43,20 +48,30 @@ namespace T101_ConsolidatedEndpoints.Controllers
 		[HttpPut]
 		public IActionResult UpsertUser(UserModel userComplete)
 		{
-			string sql = @$"
+			string sql = @"
 			EXEC TutorialAppSchema.spUser_Upsert
-				@FirstName = '{userComplete.FirstName}',
-				@LastName = '{userComplete.LastName}',
-				@Email = '{userComplete.Email}',
-				@Gender = '{userComplete.Gender}',
-				@Active = '{userComplete.Active}',
-				@JobTitle = '{userComplete.JobTitle}',
-				@Department = '{userComplete.Department}',
-				@Salary = '{userComplete.Salary.ToString(_specifier, _culture)}',
-				@UserId = {userComplete.UserId}
+				@FirstName = @FirstNameParam,
+				@LastName = @LastNameParam,
+				@Email = @EmailParam,
+				@Gender = @GenderParam,
+				@Active = @ActiveParam,
+				@JobTitle = @JobTitleParam,
+				@Department = @DepartmentParam,
+				@Salary = @SalaryParam,
+				@UserId = @UserIdParam
 			";
+			DynamicParameters sqlParameters = new();
+			sqlParameters.Add("@FirstNameParam", userComplete.FirstName, DbType.String);
+			sqlParameters.Add("@LastNameParam", userComplete.LastName, DbType.String);
+			sqlParameters.Add("@EmailParam", userComplete.Email, DbType.String);
+			sqlParameters.Add("@GenderParam", userComplete.Gender, DbType.String);
+			sqlParameters.Add("@ActiveParam", userComplete.Active, DbType.Boolean);
+			sqlParameters.Add("@JobTitleParam", userComplete.JobTitle, DbType.String);
+			sqlParameters.Add("@DepartmentParam", userComplete.Department, DbType.String);
+			sqlParameters.Add("@SalaryParam", userComplete.Salary, DbType.Decimal);
+			sqlParameters.Add("@UserIdParam", userComplete.UserId, DbType.Int32);
 
-			if (_dapper.ExecuteSql(sql))
+			if (_dapper.ExecuteSqlWithParameters(sql, sqlParameters))
 			{
 				return Ok();
 			}
@@ -68,9 +83,11 @@ namespace T101_ConsolidatedEndpoints.Controllers
 		[HttpDelete("{userId}")]
 		public IActionResult DeleteUser(int userId)
 		{
-			string sql = $"EXEC TutorialAppSchema.spUser_Delete @UserId={userId}";
+			string sql = $"EXEC TutorialAppSchema.spUser_Delete @UserId=@UserIdParam";
+			DynamicParameters sqlParameters = new();
+			sqlParameters.Add("@UserIdParam", userId, DbType.Int32);
 
-			if (_dapper.ExecuteSql(sql))
+			if (_dapper.ExecuteSqlWithParameters(sql, sqlParameters))
 			{
 				return Ok();
 			}
